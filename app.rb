6f087e9bb1941bb0d60df5005cdf0dc06d8bc5f1
Joe Ganobsik <jganobsik@gmail.com>
2019-01-25 01:50:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-   get '/' do
+  
+  get '/' do
     erb :user_input
   end
 
+
    post '/piglatinize' do
-     
      
     l = PigLatinizer.new
     
